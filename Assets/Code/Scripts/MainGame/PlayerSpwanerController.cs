df63d25d6f63259ee2941fa38e54ef58aa087b21
Esteban González <juanesgs2002@gmail.com>
2024-01-09 14:21:53 -0500

    System and logic to create, spawn, and despawn players in the rooms

---
+++ Assets/Code/Scripts/MainGame/PlayerSpwanerController.cs
@@ -0,0 +1,57 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpwanerController : NetworkBehaviour, IPlayerJoined, IPlayerLeft
+{
+    [SerializeField] private NetworkPrefabRef playerNetworkPrefab;
+    [SerializeField] private Transform[] spawnPoints;
+
+    public override void Spawned()
+    {
+        if (Runner.IsServer)
+        {
+            foreach(var item in Runner.ActivePlayers)
+            {
+                SpawnPlayer(item);
+            }
+        }
+    }
+
+    private void SpawnPlayer(PlayerRef playerRef)
+    {
+        if (Runner.IsServer)
+        {
+            var index = playerRef % spawnPoints.Length;
+            var spawnPoint = spawnPoints[index].transform.position;
+            var playerObj = Runner.Spawn(playerNetworkPrefab, spawnPoint, Quaternion.identity, playerRef);
+
+            Runner.SetPlayerObject(playerRef, playerObj);
+        }
+    }
+
+    private void DespawnPlayer(PlayerRef playerRef)
+    {
+        if(Runner.IsServer)
+        {
+            if(Runner.TryGetPlayerObject(playerRef, out var playerNetwornObject))
+            {
+                Runner.Despawn(playerNetwornObject);
+            }
+
+            //Reset player object
+            Runner.SetPlayerObject(playerRef, null);
+        }
+    }
+
+    public void PlayerJoined(PlayerRef player)
+    {
+        SpawnPlayer(player);
+    }
+
+    public void PlayerLeft(PlayerRef player)
+    {
+        DespawnPlayer(player);
+    }
+}
--- Assets/Code/Scripts/Others/GlobalManagers.cs
+++ Assets/Code/Scripts/Others/GlobalManagers.cs
@@ -20,6 +20,4 @@ public class GlobalManagers : MonoBehaviour
             Destroy(parentObj);
         }
     }
-
-
 }
